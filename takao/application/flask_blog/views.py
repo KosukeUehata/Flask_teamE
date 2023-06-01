31a6a7efd5eefca9ac2c99eadc0f2cd1481adfc7
TakaoYuna <takao.yuna.ai@mynavi.jp>
2023-06-01 04:48:11 +0000

    htmlファイルを初めて利用

--- takao/application/flask_blog/views.py
+++ takao/application/flask_blog/views.py
@@ -1,6 +1,7 @@
 # ビューファイル
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app # __init__.pyで作成したappをインポート
 
 @app.route('/') # urlにアクセスがあった際の処理(アクセスがあったら、show_entries()というメゾットが呼び出される)
 def show_entries():
-    return "Hello World"
+    return render_template('entries/index.html') 
