22fc0dc0733935c7f9bfb96e300d044800cfd978
KosukeUehata <uehata.kosuke.dx@mynavi.jp>
2023-06-01 06:55:46 +0000

    add chap18

--- uehata/application/flask_blog/views.py
+++ uehata/application/flask_blog/views.py
@@ -1,8 +1,11 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
+# cookieのsession情報がTrueの場合はログイン画面にリダイレクトする
 @app.route("/")
 def show_entries():
+    if not session.get("logged_in"):
+        return redirect("/login")
     return render_template("entries/index.html")
 
 @app.route("/login",methods=["GET","POST"])
@@ -13,9 +16,12 @@ def login():
         elif request.form["password"] != app.config["PASSWORD"]:
             print("パスワードが異なります")
         else:
+            # ログインに成功したとき
+            session["logged_in"] = True
             return redirect("/")
     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
+    session.pop("logged_in",None)
     return redirect("/")
