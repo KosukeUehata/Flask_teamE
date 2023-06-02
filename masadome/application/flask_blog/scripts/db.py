829de8d29909e9a0503e11c4ac583769667822a4
RyugoMasadome <masadome.ryugo.bt@mynavi.jp>
2023-06-02 06:44:09 +0000

    DB作成のためのファイル追加

---
+++ masadome/application/flask_blog/scripts/db.py
@@ -0,0 +1,9 @@
+from flask_script import Command
+from flask_blog.models.entries import Entry
+from flask_blog import db
+
+class InitDB(Command):
+    "create database"
+
+    def run(self):
+        db.create_all()
